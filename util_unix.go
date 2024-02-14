e7a9747fd7866b26392e16e7e3d5d4d756179285
ryanbekhen <i@ryanbekhen.dev>
2024-02-14 15:09:20 +0700

    Replace SO_REUSEADDR with SO_REUSEPORT in Unix socket settings
    The commit updates the Unix socket settings function from using the SO_REUSEADDR option to the SO_REUSEPORT option. This change is made in the `util_unix.go` file. SO_REUSEPORT is preferable as it permits multiple sockets to be bound to an identical socket address.

--- util_unix.go
+++ util_unix.go
@@ -2,10 +2,15 @@
 
 package sfu
 
+import (
+	"golang.org/x/sys/unix"
+	"syscall"
+)
+
 func setSocketOptions(network, address string, conn syscall.RawConn) error {
 	var operr error
 	if err := conn.Control(func(fd uintptr) {
-		operr = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+		operr = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 	}); err != nil {
 		return err
 	}
